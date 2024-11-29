c1cbf3870072dc228befb6df28ee98fe46b67c68
Alex Rusanovschi <65390230+alex-r-smartdata@users.noreply.github.com>
2024-11-29 21:21:23 +0200

    WLP-2712: updated publication script; added source .aar;

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,31 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("maven-publish")
 }
 
+val githubProperties = Properties()
+githubProperties.load(FileInputStream(rootProject.file("github.properties")))
+
+publishing {
+    publications {
+        register<MavenPublication>("release"){
+            groupId = "com.paymenttools"
+            artifactId = "paymenttoolssdk"
+            version = "1.0.11"
+            artifact("source/paymenttools-sdk-release.aar")
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/paymenttools/wlp-android-distribution")
+            credentials{
+                username = githubProperties.getProperty("gpr.usr")
+                password = githubProperties.getProperty("gpr.key")
+            }
+        }
+    }
+}
